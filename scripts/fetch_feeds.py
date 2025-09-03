48f9fd95a5d97401c284dc4dc6464ecbfd8d1778
tinykings <92499963+tinykings@users.noreply.github.com>
2025-09-03 15:08:38 -0700

    Update fetch_feeds.py

--- scripts/fetch_feeds.py
+++ scripts/fetch_feeds.py
@@ -19,7 +19,7 @@ warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 
 # Configuration
 TIMEZONE = 'America/Los_Angeles'
-ITEMS_RETENTION_DAYS = 5
+ITEMS_RETENTION_DAYS = 7
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 REQUEST_TIMEOUT = 30
 
